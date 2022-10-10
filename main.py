f5b40020968fb45f80781f580ae4d6894cdd161f
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-09 23:58:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ numbers = list(map(int, input().split()))
 # ******************************
 # Make your Code
 # ******************************
+numbers = input().split()
 
+for i in range(len(numbers)):
+    numbers[i] = int(numbers[i])
+
+sum_num = 0
+for j in numbers:
+    sum_num += j
+
+avg = sum_num / len(numbers)
+
+# getting the differences
+for k in numbers:
+    print(f"{abs(avg - k):.2f}", end=" ")
 
 # Use this statement to print out the list element. # Replace the variable 'dist' with your variable
 # print (f'{dist:.2f}', end=' ')
